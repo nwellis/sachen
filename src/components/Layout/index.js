71debfde6c13cfe9b0e6460b5427b3b2e9221152
Nick Ellis <nicholas.w.ellis@gmail.com>
2020-04-11 17:11:25 -0500

    chore(deps): upgrade

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,6 +10,7 @@ import About from "../About";
 import NavBar, { TabKeys } from "../NavBar";
 import about from "data/about.json";
 import Gallery from "../Gallery";
+import StaggeredGallery from "../StaggeredGallery";
 
 const Content = () => {
   const [tab, setTab] = useState(TabKeys.Gallery);
@@ -20,7 +21,7 @@ const Content = () => {
         <NavBar selected={tab} onTabSelect={tab => setTab(tab)} />
       </HeaderSection>
       <ContentSection>
-        {tab === TabKeys.Gallery && <Gallery />}
+        {tab === TabKeys.Gallery && <StaggeredGallery />}
         {tab === TabKeys.Contact && (
           <Container>
             <About paragraphs={about} />
